16ad3a4a299b770afcb78aa1f90d9465675ad748
cory <cory@666.com>
2023-03-04 17:02:43 +0800

    4 master

--- src/A.java
+++ src/A.java
@@ -1,6 +1,7 @@
 public class A {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("merge");
         System.out.println("second commit");
     }
 }
